c62d69150694143c769e2a52864169f590fced9e
Tom Mowad <tmowad@gmail.com>
2014-06-04 13:22:30 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EmailListBuilder::Application.config.secret_key_base = '90ff8bff78faed255a3890db96346a4fb25ea5b9bd68c949f31b2d7346f806191ab5e9535a05271181ba5391e47f206278fdcf926f6b2c90f58f7133c6cbac11'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+EmailListBuilder::Application.config.session_store :cookie_store, key: '_email-list-builder_session'
